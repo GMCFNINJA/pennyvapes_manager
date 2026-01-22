cbd2eda0d0a98d85e37d1eac8f3752a1d23ae589
GMCFNINJA <96871750+GMCFNINJA@users.noreply.github.com>
2026-01-22 00:40:31 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,1 +1,11 @@
+// supabase.js
+const SUPABASE_URL = "https://izzfsjzdatctzrqwukvx.supabase.co";
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6emZzanpkYXRjdHpycXd1a3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMzc5NzAsImV4cCI6MjA4NDYxMzk3MH0.UgEhFl2nl9L6riefYC0AxqvyOV9D-ZwWBMaQln8MjWg";
+
+const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+
+// login por "username" -> email fake
+function usernameToEmail(username) {
+  return `${username}@penny.local`;
+}
 
