36bfabb75d7cbbc888db79f82cc1604fc98289c7
GMCFNINJA <96871750+GMCFNINJA@users.noreply.github.com>
2026-01-22 00:52:34 +0000

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -2,10 +2,9 @@
 const SUPABASE_URL = "https://izzfsjzdatctzrqwukvx.supabase.co";
 const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml6emZzanpkYXRjdHpycXd1a3Z4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkwMzc5NzAsImV4cCI6MjA4NDYxMzk3MH0.UgEhFl2nl9L6riefYC0AxqvyOV9D-ZwWBMaQln8MjWg";
 
-const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+// IMPORTANTe: cria o client com outro nome (sb), não "supabase"
+window.sb = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
 
-// login por "username" -> email fake
-function usernameToEmail(username) {
+window.usernameToEmail = function (username) {
   return `${username}@penny.local`;
-}
-
+};
